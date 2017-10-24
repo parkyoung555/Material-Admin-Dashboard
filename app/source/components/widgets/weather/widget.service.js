aecad75621f68af3da06e086917648b8f801c879
Chang Oscar <oscarchang1226@gmail.com>
2017-10-23 21:33:49 -0500

    update weather requested by boss

--- app/source/components/widgets/weather/widget.service.js
+++ app/source/components/widgets/weather/widget.service.js
@@ -5,11 +5,27 @@
     .service('weatherWidgetService', weatherWidgetService);
 
   function weatherWidgetService($http, dummyData) {
-    this.getData = getData;
 
-    function getData() {
-      return dummyData;
+    this.getWeatherData = getWeatherData;
+
+    // YQL = Yahoo Query Language
+    function getYQL(text) {
+      var woeidQ = 'select woeid from geo.places(1) where text="'+text+'"';
+      return 'select * from weather.forecast where woeid in ('+woeidQ+')';
     }
+
+    // Example for text: vancouver, wa
+    function getWeatherData(text) {
+      return $http({
+        method: 'GET',
+        url: 'https://query.yahooapis.com/v1/public/yql',
+        params: {
+          q: getYQL(text),
+          format: 'json'
+        }
+      });
+    }
+
   }
     weatherWidgetService.$inject = ['$http', 'dummyData'];
 })();
